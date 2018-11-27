28bf0afef9c6d29bb3a0bca0d302df2e80f0fe1b
Dana Gajewska <mdgajewska@outlook.com>
2018-11-27 14:45:05 +0000

    Adds up and down functions

--- spec/features/thermostat_spec.js
+++ spec/features/thermostat_spec.js
@@ -1,8 +1,27 @@
 'use strict';
 
 describe('Thermostat', function() {
+  var thermostat;
+  var default_temp;
+  beforeEach(function() {
+    thermostat = new Thermostat();
+    default_temp = thermostat.reading();
+  });
   it('is set to 20 by default', function() {
-    var thermostat = new Thermostat();
     expect(thermostat.reading()).toEqual(20);
+  });
+
+  describe('Up function',function() {
+    it('increases temperature by 1', function() {
+      thermostat.up();
+      expect(thermostat.reading()).toEqual(default_temp + 1);
+    });
+  });
+
+  describe('Down function', function() {
+    it('decreases temperature by 1', function() {
+      thermostat.down();
+      expect(thermostat.reading()).toEqual(default_temp - 1);
+    });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,3 +2,5 @@ function Thermostat() {
   this._temperature = 20;
 }
 Thermostat.prototype.reading = function() { return this._temperature; }
+Thermostat.prototype.up = function () { this._temperature++; };
+Thermostat.prototype.down = function () { this._temperature--; };

e050e2c11f43dde0eeb335d810af9862dc748312
Al <>
2018-11-27 15:12:42 +0000

    Implement minimum and maximum temperatures and power saving

--- spec/features/thermostat_spec.js
+++ spec/features/thermostat_spec.js
@@ -8,7 +8,7 @@ describe('Thermostat', function() {
     default_temp = thermostat.reading();
   });
   it('is set to 20 by default', function() {
-    expect(thermostat.reading()).toEqual(20);
+    expect(thermostat.reading()).toEqual(default_temp);
   });
 
   describe('Up function',function() {
@@ -18,10 +18,32 @@ describe('Thermostat', function() {
     });
   });
 
+  describe('Power saving', function() {
+    it('is on by default', function() {
+      expect(thermostat.isPowerSavingOn()).toBe(true);
+    });
+
+    it('limits maximum temperature to 25 when on', function() {
+      while(thermostat.reading() < 25) {
+        thermostat.up();
+      }
+      thermostat.up();
+      expect(thermostat.reading()).toEqual(25);
+    });
+  });
+
   describe('Down function', function() {
     it('decreases temperature by 1', function() {
       thermostat.down();
       expect(thermostat.reading()).toEqual(default_temp - 1);
+    });
+
+    it('has a minimum temperature of 10', function() {
+      while(thermostat.reading() > 10) {
+        thermostat.down();
+      }
+      thermostat.down();
+      expect(thermostat.reading()).toEqual(10);
     });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,26 @@
 function Thermostat() {
   this._temperature = 20;
+  this._minimumTemperature = 10;
+  this._maximumTemperature = 25;
+  this._powerSavingMode = true;
 }
-Thermostat.prototype.reading = function() { return this._temperature; }
-Thermostat.prototype.up = function () { this._temperature++; };
-Thermostat.prototype.down = function () { this._temperature--; };
+Thermostat.prototype.reading = function() {
+  return this._temperature;
+}
+Thermostat.prototype.up = function () {
+  if(this._temperature < this._maximumTemperature) {
+    this._temperature++;
+  }
+};
+Thermostat.prototype.down = function () {
+  if(this._temperature > this._minimumTemperature) {
+    this._temperature--;
+  }
+};
+Thermostat.prototype.powerSavingOn = function () {
+  this._maximumTemperature = 25;
+  this._powerSavingMode = true;
+};
+Thermostat.prototype.isPowerSavingOn = function() {
+  return this._powerSavingMode;
+};
